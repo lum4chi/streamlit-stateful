f88d460d99be3d0805d0cd4e8ecbe9ba68464861
lum4chi <francesco.lumachi@gmail.com>
2023-11-02 15:18:22 +0100

    Import fix

--- st_stateful/__init__.py
+++ st_stateful/__init__.py
@@ -1,4 +1,5 @@
 from .data_editor import stateful_data_editor
+from .file_uploader import stateful_file_uploader
 from .multiselect import stateful_multiselect
 from .number_input import stateful_number_input
 from .selectbox import stateful_selectbox
