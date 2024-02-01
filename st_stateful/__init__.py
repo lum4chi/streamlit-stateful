fe80cb40b699212e7723f5dc59ed462df27bd564
lum4chi <francesco.lumachi@gmail.com>
2024-02-01 10:40:15 +0100

    Checkbox resolution

--- st_stateful/__init__.py
+++ st_stateful/__init__.py
@@ -4,3 +4,4 @@ from .multiselect import stateful_multiselect
 from .number_input import stateful_number_input
 from .selectbox import stateful_selectbox
 from .slider import stateful_slider
+from .checkbox import stateful_checkbox
