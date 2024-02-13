e89fdc5c761d5ba1452f974134b8160915f29fbf
lum4chi <francesco.lumachi@gmail.com>
2024-02-13 11:53:21 +0100

    Widgets added

---
+++ st_stateful/status.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from typing import Any, MutableMapping
+from streamlit.type_util import Key
+from streamlit.delta_generator import DeltaGenerator
+
+
+def stateful_status(
+    label: str,
+    key: str,
+    *args,
+    expanded: bool = False,
+    session: MutableMapping[Key, Any] = st.session_state,
+    position: DeltaGenerator = st._main,
+    **kwargs,
+):
+    """
+    A stateful that preserves expanded status.
+    """
+    # TODO not working properly, miss a way to update expanded status when clicked
+    if f"{key}.expanded" not in session:
+        session[f"{key}.expanded"] = expanded
+
+    return position.status(
+        label,
+        *args,
+        expanded=session[f"{key}.expanded"],
+        **kwargs,
+    )
