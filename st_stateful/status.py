1664d6b8a06df9e79d86f563e9af9545bfdc4c9d
lum4chi <francesco.lumachi@gmail.com>
2024-02-13 11:18:53 +0100

    Status added

---
+++ st_stateful/status.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from typing import Any, MutableMapping
+from streamlit.type_util import Key
+from streamlit.delta_generator import DeltaGenerator
+
+
+def stateful_status(
+    key: str,
+    *args,
+    expanded: bool = False,
+    session: MutableMapping[Key, Any] = st.session_state,
+    position: DeltaGenerator = st._main,
+    **kwargs,
+):
+    """
+    A stateful that preserves expanded status.
+    """
+    if f"{key}.value" not in session:
+        session[f"{key}.expanded"] = expanded
+
+    return position.status(
+        *args,
+        expanded=session[f"{key}.expanded"],
+        **kwargs,
+    )
