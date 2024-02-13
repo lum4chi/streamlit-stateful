8c758aa78ef6a795fc42eacce778fec0606f3655
lum4chi <francesco.lumachi@gmail.com>
2024-02-13 11:18:49 +0100

    Plotly chart added

---
+++ st_stateful/plotly_chart.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from streamlit.delta_generator import DeltaGenerator
+from streamlit.elements.plotly_chart import FigureOrData
+
+
+def stateful_plotly_chart(
+    figure_or_data: FigureOrData, *args, position: DeltaGenerator = st._main, **kwargs
+):
+    """
+    A stateful plotly chart that preserves zooming.
+    Source: https://github.com/streamlit/streamlit/issues/6324
+    """
+    figure_or_data.update_layout({"uirevision": "foo"}, overwrite=True)
+    return position.plotly_chart(
+        figure_or_data,
+        *args,
+        **kwargs,
+    )
